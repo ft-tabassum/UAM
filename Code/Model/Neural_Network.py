b4fe3ed008f3e7917bf25daa117cf608a2c9641b
ft-tabassum <fariha.tabassum@tum.de>
2025-06-19 15:27:34 +0200

    Initial commit

--- Code/Model/Neural_Network.py
+++ Code/Model/Neural_Network.py
@@ -426,6 +426,6 @@ with open('../../Result/Prediction_EvaluationMetrics/Result_NeuralNetwork.txt', 
 
 # Save confusion matrix
 conf_matrix_df = pd.DataFrame(test_cm, index=classes, columns=classes)
-conf_matrix_df.to_csv('CM_confusion_matrix_NeuralNetwork.csv')
+conf_matrix_df.to_csv('CM_NeuralNetwork.csv')
 
 logger.info("Cross-validation completed. Results saved to Result_NeuralNetwork.txt") 
